33556b197dbba447309b6aed8f80865a20aa772b
Billy Kirk <billy@billys-mbp.lan>
2019-05-13 21:53:49 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+
+  // load the dist folder from Angular
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/electron-test/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  // The following is optional and will open the DevTools:
+  // win.webContents.openDevTools()
+
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+// on macOS, closing the window doesn't quit the app
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// initialize the app's main window
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
