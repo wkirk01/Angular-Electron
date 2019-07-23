f3a697879515286defaf8987933bf171c749042c
William Kirk <billykirk@williams-mbp.lan>
2019-07-22 21:20:31 -0500

    Updates

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ require('electron-reload')(__dirname, {
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({
+    width: 800, height: 600, webPreferences: {
+      nodeIntegration: true,
+    }
+  });
 
   // load the dist folder from Angular
   win.loadURL(
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { ElectronService } from 'ngx-electron';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'electron-test';
+
+  constructor(private electronService: ElectronService) { }
+
+  public buttonClicked() {
+    if (this.electronService.isElectronApp) {
+      this.electronService.remote.dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] });
+    }
+  }
+
 }
